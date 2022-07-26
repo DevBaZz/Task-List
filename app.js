8f046455791f9a5a53fd320182532331db6b9101
BaZz <bazuaye50@gmail.com>
2022-07-26 04:44:18 +0100

    ....

--- app.js
+++ app.js
@@ -3,5 +3,33 @@ function getSingleHTMLElement(element) {
     return document.querySelector(element); // getting element from the DOM
 }
 ;
+function createHTMLElement(ele) {
+    return document.createElement(ele);
+}
 const form = getSingleHTMLElement('.form');
-const task = getSingleHTMLElement('.collections');
+const tasklist = getSingleHTMLElement('.collections');
+const clearTaskBTN = getSingleHTMLElement('.cleartask');
+const filter = getSingleHTMLElement('#filter');
+const taskInput = getSingleHTMLElement('#task');
+// Load all events function
+loadAllEvent();
+// Events Function
+function loadAllEvent() {
+    form === null || form === void 0 ? void 0 : form.addEventListener('submit', addtask);
+}
+function addtask(e) {
+    const taskValue = (taskInput.value).trim();
+    if (taskValue === '') {
+        return alert('This is empty taskInput');
+    }
+    // Create HTML element
+    const li = createHTMLElement('li');
+    const liValue = document.createTextNode(taskValue);
+    // Appending Elements
+    li.classList.add('collection-item');
+    li.appendChild(liValue);
+    tasklist.appendChild(li);
+    // Clear
+    taskInput.value = '';
+    e.preventDefault();
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,40 @@
 function getSingleHTMLElement(element:string):HTMLElement | null  {
     return document.querySelector(element);// getting element from the DOM
 };
+function createHTMLElement(ele:string):HTMLElement{
+    return document.createElement(ele);
+}
 
-const form = getSingleHTMLElement('.form');
-const task =  getSingleHTMLElement('.collections');
+const form = getSingleHTMLElement('.form') as HTMLFormElement;
+const tasklist =  getSingleHTMLElement('.collections') as HTMLUListElement;
+const clearTaskBTN = getSingleHTMLElement('.cleartask');
+const filter = getSingleHTMLElement('#filter');
+const taskInput = getSingleHTMLElement('#task') as HTMLInputElement;
+
+// Load all events function
+loadAllEvent()
+
+// Events Function
+function loadAllEvent(){
+    form?.addEventListener('submit', addtask);
+}
+
+function addtask(e:any){
+    const taskValue = (taskInput.value).trim();
+    if(taskValue === ''){
+        return  alert('This is empty taskInput');
+    }
+    // Create HTML element
+    const li = createHTMLElement('li');
+    const liValue = document.createTextNode(taskValue);
+
+    // Appending Elements
+    li.classList.add('collection-item');
+    li.appendChild(liValue);
+    tasklist.appendChild(li);
+
+    // Clear
+    taskInput.value = '';
+    
+    e.preventDefault();
+}
